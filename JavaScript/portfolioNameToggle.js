407bd4415eb62cba9dbbad997bdd4724dc967c57
Caleb Tony <calebmbugua@gmail.com>
2021-03-01 02:02:29 +0300

    Completed portfolio hover effect and linked it to index.html

---
+++ JavaScript/portfolioNameToggle.js
@@ -0,0 +1,56 @@
+$("#work1-image")
+  .mouseenter(() => {
+    $("#work1-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work1-name").addClass("hidden");
+  });
+$("#work2-image")
+  .mouseenter(() => {
+    $("#work2-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work2-name").addClass("hidden");
+  });
+$("#work3-image")
+  .mouseenter(() => {
+    $("#work3-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work3-name").addClass("hidden");
+  });
+$("#work4-image")
+  .mouseenter(() => {
+    $("#work4-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work4-name").addClass("hidden");
+  });
+$("#work5-image")
+  .mouseenter(() => {
+    $("#work5-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work5-name").addClass("hidden");
+  });
+$("#work6-image")
+  .mouseenter(() => {
+    $("#work6-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work6-name").addClass("hidden");
+  });
+$("#work7-image")
+  .mouseenter(() => {
+    $("#work7-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work7-name").addClass("hidden");
+  });
+$("#work8-image")
+  .mouseenter(() => {
+    $("#work8-name").removeClass("hidden");
+  })
+  .mouseleave(() => {
+    $("#work8-name").addClass("hidden");
+  });
